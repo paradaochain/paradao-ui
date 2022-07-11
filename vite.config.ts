cf71d9e7a68e7e0bdfa4e52a36a4f4ee192a4d1a
j0nl1 <jonlitech@gmail.com>
2022-07-11 05:08:03 +0200

    dependencies-fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,37 @@ export default defineConfig({
         plugins: ['babel-plugin-macros', 'babel-plugin-styled-components']
       }
     }),
-    tsconfigPaths()
-  ]
+    tsconfigPaths(),
+    NodeGlobalsPolyfillPlugin({
+      process: true,
+      buffer: true
+    }),
+    NodeModulesPolyfillPlugin()
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill()
+      ]
+    }
+  }
 });
